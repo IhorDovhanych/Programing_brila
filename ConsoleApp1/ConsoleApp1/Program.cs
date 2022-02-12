e7ed179a32cfea6a69379e11e58f5f5ce433d501
Ihor <dovhanych.ihor@student.uzhnu.edu.ua>
2022-02-12 15:42:07 +0200

    .

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("first_num = ");
+            int first_num = int.Parse(Console.ReadLine());
+            Console.Write("second_num = ");
+            int second_num = int.Parse(Console.ReadLine());
+            int sum = first_num + second_num;
+            Console.WriteLine(sum);
+
+        }
+    }
+}
